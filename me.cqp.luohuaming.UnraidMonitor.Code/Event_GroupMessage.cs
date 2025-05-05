bf81c854310463768d8406fdc5919b3fbf806783
Mr.喜 <863450594@qq.com>
2025-05-05 15:08:08 +0800

    写入插件信息

---
+++ me.cqp.luohuaming.UnraidMonitor.Code/Event_GroupMessage.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using me.cqp.luohuaming.UnraidMonitor.Sdk.Cqp.EventArgs;
+using me.cqp.luohuaming.UnraidMonitor.PublicInfos;
+
+namespace me.cqp.luohuaming.UnraidMonitor.Code
+{
+    public class Event_GroupMessage
+    {
+        public static FunctionResult GroupMessage(CQGroupMessageEventArgs e)
+        {
+            FunctionResult result = new FunctionResult()
+            {
+                SendFlag = false
+            };
+            try
+            {
+                foreach (var item in MainSave.Instances.OrderByDescending(x => x.Priority)
+                    .Where(item => item.CanExecute(e.Message.Text)))
+                {
+                    var r = item.Execute(e);
+                    if (r.Result)
+                    {
+                        return r;
+                    }
+                }
+                return result;
+            }
+            catch (Exception exc)
+            {
+                MainSave.CQLog.Info("异常抛出", exc.Message + exc.StackTrace);
+                return result;
+            }
+        }
+    }
+}
---
+++ me.cqp.luohuaming.UnraidMonitor.Code/Event_PrivateMessage.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using me.cqp.luohuaming.UnraidMonitor.Sdk.Cqp.EventArgs;
+using me.cqp.luohuaming.UnraidMonitor.PublicInfos;
+
+namespace me.cqp.luohuaming.UnraidMonitor.Code
+{
+    public class Event_PrivateMessage
+    {
+        public static FunctionResult PrivateMessage(CQPrivateMessageEventArgs e)
+        {
+            FunctionResult result = new FunctionResult()
+            {
+                SendFlag = false
+            };
+            try
+            {
+                foreach (var item in MainSave.Instances.OrderByDescending(x => x.Priority)
+                    .Where(item => item.CanExecute(e.Message.Text)))
+                {
+                    var r = item.Execute(e);
+                    if (r.Result)
+                    {
+                        return r;
+                    }
+                }
+                return result;
+            }
+            catch (Exception exc)
+            {
+                MainSave.CQLog.Info("异常抛出", exc.Message + exc.StackTrace);
+                return result;
+            }
+        }
+    }
+}
