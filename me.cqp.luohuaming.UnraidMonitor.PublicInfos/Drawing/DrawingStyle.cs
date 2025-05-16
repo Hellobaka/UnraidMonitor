012a78d18d7033a4994f9756de44efdd4d5a62bf
Mr.喜 <863450594@qq.com>
2025-05-16 15:54:59 +0800

    绘图基板类

---
+++ me.cqp.luohuaming.UnraidMonitor.PublicInfos/Drawing/DrawingStyle.cs
@@ -0,0 +1,49 @@
+﻿using SkiaSharp;
+using System;
+
+namespace me.cqp.luohuaming.UnraidMonitor.PublicInfos.Drawing
+{
+    public class DrawingStyle
+    {
+        public string Name { get; set; }
+
+        public DrawingBase[] Content { get; set; }
+
+        public DateTime CreateTime { get; set; }
+
+        public DateTime ModifyTime { get; set; }
+
+        public enum BackgroundType
+        {
+            Color,
+
+            Image
+        }
+
+        public enum BackgroundImageScaleType
+        {
+            Center,
+
+            Stretch,
+
+            Tile,
+
+            Fill
+        }
+
+        public SKColor BackgrdoundColor { get; set; }
+
+        public string[] BackgroundImages { get; set; } = [];
+
+        public BackgroundType DrawBackgroundType { get; set; } = BackgroundType.Color;
+
+        public BackgroundImageScaleType DrawBackgroundImageScaleType { get; set; } = BackgroundImageScaleType.Fill;
+
+        public double BackgroundBlur { get; set; } = 0;
+
+        public Painting Draw(int width, int height)
+        {
+            return null;
+        }
+    }
+}
