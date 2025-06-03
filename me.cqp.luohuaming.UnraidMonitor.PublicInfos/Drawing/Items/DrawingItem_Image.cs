e57afd7010df10c7fe6693a0c80058ae6e26b50a
Mr.喜 <863450594@qq.com>
2025-06-03 17:59:07 +0800

    图片样式

---
+++ me.cqp.luohuaming.UnraidMonitor.PublicInfos/Drawing/Items/DrawingItem_Image.cs
@@ -0,0 +1,74 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace me.cqp.luohuaming.UnraidMonitor.PublicInfos.Drawing.Items
+{
+    public class DrawingItem_Image : DrawingItemBase
+    {
+        public override DrawingBase.Layout Layout { get; set; } = DrawingBase.Layout.Minimal;
+
+        public override ItemType Type { get; set; } = ItemType.Image;
+
+        public string Source { get; set; }
+
+        public override float CalcHeight(DrawingStyle.Theme theme)
+        {
+            if (OverrideHeight == 0)
+            {
+                OverrideHeight = 100;
+            }
+            return base.CalcHeight(theme);
+        }
+
+        public override void ApplyBinding()
+        {
+            base.ApplyBinding();
+            if (Binding == null)
+            {
+                return;
+            }
+            if (Binding.Value.TryGetValue("Source", out var data))
+            {
+                Source = data.FormattedString;
+            }
+        }
+
+        public override (SKPoint endPoint, float width, float height) Draw(Painting painting, SKPoint startPoint, float desireWidth, DrawingStyle.Theme theme, DrawingStyle.Colors palette)
+        {
+            string pathA = Path.Combine(MainSave.ImageDirectory, Source);
+            string pathB = Path.Combine(MainSave.AppDirectory, Source);
+            string pathC = Source;
+            string path = "";
+            foreach (var item in new string[] { pathA, pathB, pathC })
+            {
+                if (File.Exists(item))
+                {
+                    path = item;
+                    break;
+                }
+            }
+
+            if (!File.Exists(path))
+            {
+                throw new FileNotFoundException($"Image source '{Source}' not found in any of the expected directories.");
+            }
+
+            painting.DrawImage(path, new SKRect
+            {
+                Location = new SKPoint(startPoint.X, startPoint.Y),
+                Size = new SKSize(desireWidth, OverrideHeight)
+            });
+
+            return (
+                endPoint: new SKPoint(startPoint.X + desireWidth, startPoint.Y + OverrideHeight),
+                width: desireWidth,
+                height: OverrideHeight
+            );
+        }
+    }
+}
