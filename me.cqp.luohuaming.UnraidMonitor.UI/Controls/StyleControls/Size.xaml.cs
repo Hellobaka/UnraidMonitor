cfb2785b45980d59f6364560c0b41887a76bf115
Mr.喜 <863450594@qq.com>
2025-07-03 15:07:24 +0800

    尺寸修改器

---
+++ me.cqp.luohuaming.UnraidMonitor.UI/Controls/StyleControls/Size.xaml.cs
@@ -0,0 +1,26 @@
+﻿using SkiaSharp;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace me.cqp.luohuaming.UnraidMonitor.UI.Controls.StyleControls
+{
+    /// <summary>
+    /// Margin.xaml 的交互逻辑
+    /// </summary>
+    public partial class Size : UserControl
+    {
+        public Size()
+        {
+            InitializeComponent();
+        }
+
+        public SKSize SKSize
+        {
+            get { return (SKSize)GetValue(SizeProperty); }
+            set { SetValue(SizeProperty, value); }
+        }
+
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.Register("SKSize", typeof(SKSize), typeof(Size), new PropertyMetadata(new SKSize()));
+    }
+}
