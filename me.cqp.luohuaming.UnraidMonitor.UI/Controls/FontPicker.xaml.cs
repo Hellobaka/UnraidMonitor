cbe86fc29ac6575e2f2f1edbe0dbd751394fa260
Mr.喜 <863450594@qq.com>
2025-07-03 17:31:12 +0800

    字体预览器

---
+++ me.cqp.luohuaming.UnraidMonitor.UI/Controls/FontPicker.xaml.cs
@@ -0,0 +1,90 @@
+﻿using me.cqp.luohuaming.UnraidMonitor.PublicInfos.Drawing;
+using me.cqp.luohuaming.UnraidMonitor.UI.Converters;
+using Microsoft.Win32;
+using PropertyChanged;
+using SkiaSharp;
+using System;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace me.cqp.luohuaming.UnraidMonitor.UI.Controls
+{
+    /// <summary>
+    /// FontPicker.xaml 的交互逻辑
+    /// </summary>
+    [AddINotifyPropertyChangedInterface]
+    public partial class FontPicker : UserControl
+    {
+        public FontPicker()
+        {
+            InitializeComponent();
+        }
+
+        public bool Loading { get; set; } = false;
+
+        private string FontPreviewText { get; set; } = "AaBb114514 喵喵喵～(￣▽￣～)";
+
+        public string CustomFont
+        {
+            get => (string)GetValue(CustomFontProperty);
+            set => SetValue(CustomFontProperty, value);
+        }
+
+        public static readonly DependencyProperty CustomFontProperty =
+            DependencyProperty.Register("CustomFont", typeof(string), typeof(FontPicker), new PropertyMetadata("微软雅黑"));
+
+        private void BrowserFont_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new OpenFileDialog()
+            {
+                Multiselect = false,
+                Filter = "字体文件|*.ttf;*.otf;*.woff;*.woff2|所有文件|*.*",
+                CheckFileExists = true,
+            };
+            if (dialog.ShowDialog() ?? false)
+            {
+                string fontPath = dialog.FileName;
+                if (File.Exists(fontPath))
+                {
+                    CustomFont = fontPath;
+                }
+                else
+                {
+                    MainWindow.ShowError("所选文件不存在或不是有效的字体文件");
+                }
+            }
+        }
+
+        private void Popup_Closed(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void FontPreviewButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (Loading || FontPreviewPopup.IsOpen)
+            {
+                return;
+            }
+            Loading = true;
+            FontPreviewPopup.IsOpen = true;
+            string font = CustomFont;
+            using Painting painting = new(500, 100);
+            await Task.Run(() =>
+            {
+                painting.Clear(SKColors.Transparent);
+                var p = painting.DrawText(FontPreviewText, Painting.Anywhere, new(), SKColors.Black, customFont: Painting.CreateCustomFont(font));
+                painting.Resize((int)(p.X + 10), (int)(p.Y + 5));
+            });
+            var bitmapSource = (BitmapSource)SKImageToImageSource.Convert(painting);
+            Loading = false;
+            FontPreview.Source = bitmapSource;
+        }
+    }
+}
