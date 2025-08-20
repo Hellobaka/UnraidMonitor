f5281e39a6bdcd715e3fb889ad3cdb873800d14a
Mr.喜 <863450594@qq.com>
2025-08-20 16:42:36 +0800

    尝试捕获UI异常

--- me.cqp.luohuaming.UnraidMonitor.UI/App.xaml.cs
+++ me.cqp.luohuaming.UnraidMonitor.UI/App.xaml.cs
@@ -1,3 +1,5 @@
+using me.cqp.luohuaming.UnraidMonitor.PublicInfos;
+using System;
 using System.Windows;
 
 namespace me.cqp.luohuaming.UnraidMonitor.UI
@@ -11,8 +13,29 @@ namespace me.cqp.luohuaming.UnraidMonitor.UI
 
         protected override void OnStartup(StartupEventArgs e)
         {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+            
             base.OnStartup(e);
             Debug = e.Args.Length > 0 && e.Args[0] == "Debug";
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            if (e.ExceptionObject is Exception ex)
+            {
+                MainSave.CQLog?.Error("UI异常", ex.ToString());
+            }
+            else
+            {
+                MainSave.CQLog?.Error("UI异常", e.ExceptionObject?.ToString() ?? "未知错误");
+            }
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            e.Handled = true;
+            MainSave.CQLog?.Error("UI异常", e.Exception.ToString());
         }
     }
 }
