c0dd176cd02f6c811ae3ca5a1ae3cd6d831fe8b6
Mr.喜 <863450594@qq.com>
2025-08-21 14:42:40 +0800

    尝试捕获异步异常

--- me.cqp.luohuaming.UnraidMonitor.UI/App.xaml.cs
+++ me.cqp.luohuaming.UnraidMonitor.UI/App.xaml.cs
@@ -1,5 +1,6 @@
 using me.cqp.luohuaming.UnraidMonitor.PublicInfos;
 using System;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace me.cqp.luohuaming.UnraidMonitor.UI
@@ -15,9 +16,16 @@ namespace me.cqp.luohuaming.UnraidMonitor.UI
         {
             this.DispatcherUnhandledException += App_DispatcherUnhandledException;
             AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
-            
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
             base.OnStartup(e);
             Debug = e.Args.Length > 0 && e.Args[0] == "Debug";
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            e.SetObserved();
+            MainSave.CQLog?.Error("异步UI异常", e.Exception.ToString());
         }
 
         private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
@@ -35,7 +43,7 @@ namespace me.cqp.luohuaming.UnraidMonitor.UI
         private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
         {
             e.Handled = true;
-            MainSave.CQLog?.Error("UI异常", e.Exception.ToString());
+            MainSave.CQLog?.Error("UI线程异常", e.Exception.ToString());
         }
     }
 }
